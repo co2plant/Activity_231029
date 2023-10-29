b79f2d82684e637ec5fb437936ce262fa121aa24
= <=>
2023-10-29 09:38:24 +0900

    Sha

--- app/src/main/java/kr/ac/wku/activity_231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/activity_231029/MainActivity.kt
@@ -1,11 +1,15 @@
 package kr.ac.wku.activity_231029
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.ac.wku.activity_231029.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
